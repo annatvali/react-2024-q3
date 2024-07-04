4c9c235b5802665febcb28f758376ca55ddbde4b
annatvali <anntvaliashvili@gmail.com>
2024-07-05 01:19:08 +0400

    init: setup React TS boilerplate with ESLint, Prettier, Vitest, Husky, Tailwind, Vite

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import './index.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <h1 className="text-3xl font-bold text-blue-400 text-center">
+          Class Components
+        </h1>
+      </>
+    );
+  }
+}
