16d4822f5746eb359a4f0c25fe6d49b0ff3bcaab
annatvali <anntvaliashvili@gmail.com>
2024-07-07 21:30:30 +0400

    feat: add Logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+export default class Logo extends Component {
+  render() {
+    return (
+      <>
+        <p className="text-amber-300 text-4xl font-bold">Ṕoke</p>
+      </>
+    );
+  }
+}
