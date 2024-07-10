18f1800d7cbd35d311f61765dfa33fb2a9853213
annatvali <anntvaliashvili@gmail.com>
2024-07-10 17:58:27 +0400

    chore: remove commented code

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -23,21 +23,16 @@ export default class SearchBar extends Component<Props, State> {
   handleSubmit = (e: FormEvent): void => {
     e.preventDefault();
     const trimmedQuery = this.state.query.trim();
-    // this.props.onSearch(trimmedQuery);
 
     if (trimmedQuery === '') {
-      // Check if the URL already has search parameters
       const searchParams = new URLSearchParams(window.location.search);
       const hasSearchParams = searchParams.has('search');
 
       if (hasSearchParams) {
-        // If URL has search parameters, clear them and display all pokemons
         window.history.pushState({}, '', window.location.pathname);
-        this.props.onSearch(''); // Assuming onSearch with empty string fetches all pokemons
+        this.props.onSearch('');
       }
-      // If there are no search parameters, do nothing
     } else {
-      // If there is a query, proceed with the search
       this.props.onSearch(trimmedQuery);
     }
   };
