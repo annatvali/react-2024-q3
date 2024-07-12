01cd322ba64bf07ba7c507b308b4b4eeeaa26111
annatvali <anntvaliashvili@gmail.com>
2024-07-13 00:21:13 +0400

    refactor: remove unnecessary Fragment from Logo component

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -2,10 +2,6 @@ import { Component } from 'react';
 
 export default class Logo extends Component {
   render() {
-    return (
-      <>
-        <p className="text-amber-300 text-4xl font-bold">Ṕoke</p>
-      </>
-    );
+    return <p className="text-amber-300 text-4xl font-bold">Ṕoke</p>;
   }
 }
