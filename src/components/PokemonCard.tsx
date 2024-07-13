ed2d7242ad515f8c76476d085d42493dd29e881b
annatvali <anntvaliashvili@gmail.com>
2024-07-13 20:52:17 +0400

    refactor: apply React.memo to PokemonCard component for performance optimization

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,32 +1,34 @@
-import React from 'react';
+import { memo } from 'react';
 import { Pokemon } from '../types/type';
 import generateGradient from '../utils/gradientGenerator';
 
 interface PokemonCardProps extends Pokemon {}
 
-const PokemonCard: React.FC<PokemonCardProps> = ({ id, name, image, type }) => {
-  const gradientClass = generateGradient(id);
+const PokemonCard: React.FC<PokemonCardProps> = memo(
+  ({ id, name, image, type }) => {
+    const gradientClass = generateGradient(id);
 
-  return (
-    <div
-      className={`group relative my-4 cursor-pointer overflow-hidden rounded-lg shadow-md transition duration-150 ease-in-out ${gradientClass}`}
-    >
-      <div className="relative h-40 overflow-hidden rounded-md">
-        <div className="absolute top-0 right-0 bg-amber-300 py-2 px-4 rounded-md z-10">
-          <p className="text-gray-700 font-bold">{id}</p>
+    return (
+      <div
+        className={`group relative my-4 cursor-pointer overflow-hidden rounded-lg shadow-md transition duration-150 ease-in-out ${gradientClass}`}
+      >
+        <div className="relative h-40 overflow-hidden rounded-md">
+          <div className="absolute top-0 right-0 bg-amber-300 py-2 px-4 rounded-md z-10">
+            <p className="text-gray-700 font-bold">{id}</p>
+          </div>
+          <img
+            className="h-full w-full object-contain transition duration-300 ease-in-out group-hover:scale-110"
+            src={image}
+            alt={name}
+          />
         </div>
-        <img
-          className="h-full w-full object-contain transition duration-300 ease-in-out group-hover:scale-110"
-          src={image}
-          alt={name}
-        />
+        <div className="p-4">
+          <h3 className="text-xl font-bold text-gray-900 mb-2">{name}</h3>
+          <p className="text-gray-700">Type: {type}</p>
+        </div>
       </div>
-      <div className="p-4">
-        <h3 className="text-xl font-bold text-gray-900 mb-2">{name}</h3>
-        <p className="text-gray-700">Type: {type}</p>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+);
 
 export default PokemonCard;
