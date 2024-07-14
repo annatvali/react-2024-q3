da0be0d279af6673f5eeae80d86b1844500bd4b3
annatvali <anntvaliashvili@gmail.com>
2024-07-14 14:34:31 +0400

    refactor: migrate Button class component to functional component & add disabled prop

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,25 +1,30 @@
-import { Component, MouseEventHandler, ButtonHTMLAttributes } from 'react';
+import React, { MouseEventHandler, ButtonHTMLAttributes } from 'react';
 
 type Props = {
   onClick?: MouseEventHandler<HTMLButtonElement>;
   type?: ButtonHTMLAttributes<HTMLButtonElement>['type'];
-
+  disabled?: boolean;
   className?: string;
   children: React.ReactNode;
 };
 
-export default class Button extends Component<Props> {
-  render() {
-    const { onClick, type = 'button', className, children } = this.props;
+const Button: React.FC<Props> = ({
+  onClick,
+  type = 'button',
+  className,
+  children,
+  disabled = false,
+}) => {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className={`text-white p-2 rounded-md transition-colors duration-300 ${className}`}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+};
 
-    return (
-      <button
-        type={type}
-        onClick={onClick}
-        className={`text-white p-2 rounded-md transition-colors duration-300 ${className}`}
-      >
-        {children}
-      </button>
-    );
-  }
-}
+export default Button;
