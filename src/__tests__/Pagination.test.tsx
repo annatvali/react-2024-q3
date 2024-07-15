a3e22c12ab48fbeeb08d454ecb0f4d3a5c5601de
annatvali <anntvaliashvili@gmail.com>
2024-07-15 04:08:30 +0400

    feat: add tests for Pagination

---
+++ src/__tests__/Pagination.test.tsx
@@ -0,0 +1,51 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Pagination from '../components/Pagination';
+import { describe, test, expect, vi } from 'vitest';
+
+describe('Pagination Component', () => {
+  test('calls onPageChange with correct arguments when buttons are clicked', () => {
+    const onPageChangeMock = vi.fn();
+    render(
+      <Pagination
+        currentPage={3}
+        totalPages={5}
+        previousUrl="/page/2"
+        nextUrl="/page/4"
+        onPageChange={onPageChangeMock}
+      />
+    );
+    fireEvent.click(screen.getByText('Previous'));
+    expect(onPageChangeMock).toHaveBeenCalledWith(2);
+    fireEvent.click(screen.getByText('Next'));
+    expect(onPageChangeMock).toHaveBeenCalledWith(4);
+  });
+  test('disables next button on the last page and enables previous button', () => {
+    const onPageChangeMock = vi.fn();
+    render(
+      <Pagination
+        currentPage={5}
+        totalPages={5}
+        previousUrl="/page/4"
+        nextUrl={null}
+        onPageChange={onPageChangeMock}
+      />
+    );
+    expect(screen.getByRole('button', { name: 'Previous' })).toBeEnabled();
+    expect(screen.getByRole('button', { name: 'Next' })).toBeDisabled();
+  });
+  test('enables next button on the first page and disables previous button', () => {
+    const onPageChangeMock = vi.fn();
+    render(
+      <Pagination
+        currentPage={1}
+        totalPages={5}
+        previousUrl={null}
+        nextUrl="/page/2"
+        onPageChange={onPageChangeMock}
+      />
+    );
+    expect(screen.getByRole('button', { name: 'Previous' })).toBeDisabled();
+    expect(screen.getByRole('button', { name: 'Next' })).toBeEnabled();
+  });
+});
