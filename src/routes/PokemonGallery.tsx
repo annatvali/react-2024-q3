644a10c79732bff1371ced4e4635e49568d51cf1
annatvali <anntvaliashvili@gmail.com>
2024-07-26 14:28:49 +0400

    refactor(PokemonGallery): use URLSearchParams for query parameter handling, add showPagination state, update data fetching logic

--- src/routes/PokemonGallery.tsx
+++ src/routes/PokemonGallery.tsx
@@ -1,9 +1,9 @@
 import { useState, useEffect, useCallback } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate, useSearchParams } from 'react-router-dom';
 import useSearchQuery from '../hooks/useSearchQuery';
 import SearchBar from '../components/SearchBar';
-import { Pokemon, TypeInfo } from '../types/type';
-import { getPokemonsList } from '../services/api';
+import { Pokemon } from '../types/type';
+import { getPokemonsList, getPokemon } from '../services/api';
 import Pagination from '../components/Pagination';
 import CardsList from '../components/CardsList';
 import { ITEMS_PER_PAGE } from '../utils/constants';
@@ -13,39 +13,26 @@ const PokemonGallery: React.FC = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState('');
   const [totalPages, setTotalPages] = useState<number>(1);
+  const [showPagination, setShowPagination] = useState<boolean>(true);
   const [searchQuery, setSearchQuery] = useSearchQuery('searchQuery', '');
 
   const { pageId } = useParams<{ pageId: string }>();
   const currentPage = Number(pageId) || 1;
-
   const navigate = useNavigate();
-
-  const fetchPokemonDetails = async (pokemons: { url: string }[]) => {
-    return Promise.all(
-      pokemons.map(async (pokemon) => {
-        const response = await fetch(pokemon.url);
-        const details = await response.json();
-        return {
-          id: details.id,
-          name: details.name,
-          image: details.sprites.front_default,
-          type: details.types
-            .map((typeInfo: TypeInfo) => typeInfo.type.name)
-            .join(', '),
-        };
-      })
-    );
-  };
+  const [searchParams, setSearchParams] = useSearchParams();
 
   const fetchAllPokemonsData = useCallback(async (page: number) => {
     setIsLoading(true);
     try {
       const offset = (page - 1) * ITEMS_PER_PAGE;
       const pokemonsData = await getPokemonsList(offset, ITEMS_PER_PAGE);
-      const detailedPokemons = await fetchPokemonDetails(pokemonsData.results);
-      setPokemons(detailedPokemons);
+      const detailedPokemons = await Promise.all(
+        pokemonsData.results.map((pokemon) => getPokemon(pokemon.name))
+      );
+      setPokemons(detailedPokemons.filter(Boolean) as Pokemon[]);
       setTotalPages(Math.ceil(pokemonsData.count / ITEMS_PER_PAGE));
-    } catch (error) {
+      setShowPagination(true);
+    } catch {
       setError('Failed to fetch Pokémon data!');
     } finally {
       setIsLoading(false);
@@ -54,59 +41,49 @@ const PokemonGallery: React.FC = () => {
 
   const handleSearch = useCallback(
     async (query: string) => {
-      const trimmedQuery = query.trim();
       setIsLoading(true);
-      setSearchQuery(trimmedQuery);
-
-      if (trimmedQuery === '') {
+      setSearchQuery(query);
+      if (query.trim() === '') {
         fetchAllPokemonsData(currentPage);
-        window.history.pushState({}, '', window.location.pathname);
+        setSearchParams(new URLSearchParams());
         return;
       } else {
-        window.history.pushState(
-          { search: trimmedQuery },
-          '',
-          `?search=${trimmedQuery}`
-        );
+        setSearchParams({ search: query });
       }
 
       try {
         const allPokemons = await getPokemonsList(0, 1000);
         const filteredPokemons = allPokemons.results.filter((pokemon) =>
-          pokemon.name.toLowerCase().startsWith(trimmedQuery.toLowerCase())
+          pokemon.name.toLowerCase().startsWith(query.toLowerCase())
         );
-        if (filteredPokemons.length > 0) {
-          const detailedPokemons = await fetchPokemonDetails(filteredPokemons);
-          setPokemons(detailedPokemons);
-        } else {
-          setPokemons([]);
-        }
-      } catch (error) {
+        const detailedPokemons = await Promise.all(
+          filteredPokemons.map((pokemon) => getPokemon(pokemon.name))
+        );
+        setPokemons(detailedPokemons.filter(Boolean) as Pokemon[]);
+        setShowPagination(false);
+      } catch {
         setError('Failed to fetch Pokémon data!');
       } finally {
         setIsLoading(false);
       }
     },
-    [fetchAllPokemonsData, setSearchQuery, currentPage]
+    [fetchAllPokemonsData, setSearchQuery, setSearchParams, currentPage]
   );
 
   useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const initialSearchQuery = urlParams.get('search') || searchQuery;
-
-    const trimmedQuery = initialSearchQuery.trim();
-    if (trimmedQuery) {
-      handleSearch(trimmedQuery);
+    const initialSearchQuery = searchParams.get('search') || searchQuery;
+    if (initialSearchQuery) {
+      handleSearch(initialSearchQuery);
     } else {
       fetchAllPokemonsData(currentPage);
     }
-  }, [fetchAllPokemonsData, handleSearch, currentPage, searchQuery]);
-
-  useEffect(() => {
-    if (searchQuery) {
-      handleSearch(searchQuery);
-    }
-  }, [handleSearch, searchQuery]);
+  }, [
+    fetchAllPokemonsData,
+    handleSearch,
+    currentPage,
+    searchQuery,
+    searchParams,
+  ]);
 
   const handlePageChange = (pageId: number) => {
     navigate(`/page/${pageId}`);
@@ -136,7 +113,7 @@ const PokemonGallery: React.FC = () => {
             currentPage={currentPage}
             onCardClick={handleCardClick}
           />
-          {pokemons.length && (
+          {showPagination && (
             <Pagination
               currentPage={currentPage}
               totalPages={totalPages}
