f63bdcf1adb5d2ad6eb285d460b93406af7d8b56
annatvali <anntvaliashvili@gmail.com>
2024-08-15 17:33:01 +0400

    chore: setup project dependencies and tools

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: [
+      './tsconfig.json',
+      './tsconfig.node.json',
+      './tsconfig.eslint.json',
+    ],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['react-refresh', 'react-compiler'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    'react-compiler/react-compiler': 'error',
+  },
+};
